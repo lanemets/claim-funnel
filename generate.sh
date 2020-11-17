83e14f7984b239dcd4ef40485073d7edc4ede853
Valerii Lanemets <vlanemets@eastbanctech.com>
2020-11-17 10:26:12 -0500

    Generate Proto Script Fix

--- generate.sh
+++ generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd proto || return
+cd external/proto || return
 
-protoc --go_out=../external/profile/gen --go_opt=paths=source_relative \
-  --go-grpc_out=../external/profile/gen --go-grpc_opt=paths=source_relative profile.proto
+protoc --go_out=../profile/gen --go_opt=paths=source_relative \
+  --go-grpc_out=../profile/gen --go-grpc_opt=paths=source_relative profile.proto
 
 echo "Profile has been generated"
 
-protoc --go_out=../external/claim/gen --go_opt=paths=source_relative \
-  --go-grpc_out=../external/claim/gen --go-grpc_opt=paths=source_relative claim.proto
+protoc --go_out=../claim/gen --go_opt=paths=source_relative \
+  --go-grpc_out=../claim/gen --go-grpc_opt=paths=source_relative claim.proto
 
 echo "Claim has been generated"
